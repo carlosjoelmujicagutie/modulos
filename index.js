a3360d61f2e937f354c4b7e307eb3cd31c15ab4a
carlos joel mujica <carlosjoelmujica@gmail.com>
2022-07-31 14:55:03 -0500

    Update index.js

--- index.js
+++ index.js
@@ -1,4 +1,5 @@
 import {suma,multiplica} from './contoller.js'
 import chalk from 'chalk';
 console.log(suma(1,3),suma(4,5));
+console.log(multiplica(2,7));
 console.log(chalk.green(suma(1,3),suma(4,5)));
